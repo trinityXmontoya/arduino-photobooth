28dceee1449bf03842bc0cd01f484e40d8a48e8f
trinityXmontoya <trinityXmontoya@gmail.com>
2014-07-29 02:35:28 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready( function(){
+  // loadPage();
+  uploadToTwitter();
+});
+
+var loadPage = function(){
+    $.ajax({
+      url: 'http://localhost:8000/',
+      method: 'get',
+      datatype: 'json'})
+    .done(function(data){
+      scanForPics(data)}
+    )
+};
+
+var pics = [];
+
+var scanForPics = function(page){
+  pics = page.match(/<img src=".+">/g);
+  return pics
+};
+
+var uploadToTwitter = function(){
+  console.log("twittering")
+  $.ajax({
+    url: 'https://upload.twitter.com/1/statuses/update_with_media.format',
+    method: 'post',
+    datatype: 'jsonp',
+    jsonp: 'foo',
+    data: '{status: "bite me", media: "/a.png"}',
+    contentType: 'multipart/form-data'
+  })
+  .done( function(data){
+    console.log(data);
+  })
+};
+
+var foo = function(){
+  console.log("yo dog")
+}
+  // refreshPage();
+// uploadToTwitter();
---
+++ server.js
@@ -0,0 +1,28 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(9000);
+console.log('Server running at localhost:9000');
+
+
+  var OAuth = require('oauth');
+
+    var oauth = new OAuth.OAuth(
+      'https://api.twitter.com/oauth/request_token',
+      'https://api.twitter.com/oauth/access_token',
+      'your application consumer key',
+      'your application secret',
+      '1.0A',
+      null,
+      'HMAC-SHA1'
+    );
+
+    oauth.get(
+      'https://api.twitter.com/1.1/trends/place.json?id=23424977',
+      'your user token for this app', //test user token
+      'your user secret for this app', //test user secret
+      function (e, data, res){
+        if (e) console.error(e);
+        console.log(require('util').inspect(data));
+      });
